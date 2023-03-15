0f255b05fc16f1c0418ffe49b3deba6fc8db9dcd
Ben Nguyen <benb2014642@student.ctu.edu.vn>
2023-03-16 01:11:13 +0700

    16/3/2023 1:11AM

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -19,8 +19,18 @@ class IndexController extends Controller
 
     public function view_movie($slug)
     {
-        $movie = Movie::where('slug',$slug)->with('images','casts')->first();
-        dd($movie->casts[0]->character);
+        $movie = Movie::where('slug',$slug)->first();
+        // dd($movie->comments[0]->user);
         return view('user.movie.view',compact('movie'));
+    }
+
+    public function rateMovie($id, $star)
+    {
+        $movie = Movie::where('id',$id)->first();
+        $movie->votes_count += 1;
+        $temp = $movie->votes_avg + $star;
+        $movie->votes_avg = $temp/$movie->votes_count;
+        $movie->save();
+        return 'ok';
     }
 }
